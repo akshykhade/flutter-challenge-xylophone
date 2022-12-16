31032ec450dfc9f21034b9160d4c1c3fdae8c862
akshykhade <vishalpatilak@gmail.com>
2022-12-16 15:59:42 +0530

    completed XylophoneApp challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  Widget buildKey(Color color, int note) {
+    final player = AudioPlayer();
+    return Expanded(
+      child: MaterialButton(
+        onPressed: () async {
+          await player.play(AssetSource('note$note.wav'));
+        },
+        color: color,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.purple, 3),
+              buildKey(Colors.blue, 4),
+              buildKey(Colors.yellow, 5),
+              buildKey(Colors.green, 6),
+              buildKey(Colors.lightBlue, 7),
+            ],
+          ),
         ),
       ),
     );
